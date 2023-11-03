aed4e83f5fd1b28b65850dc600023b0a6750ac44
AndresLolo <alolo@correo.um.edu.uy>
2023-11-03 12:30:41 -0300

    controles con try and catch

---
+++ AeropuertoServidor/src/main/java/um/edu/uy/business/entities/Asiento.java
@@ -0,0 +1,63 @@
+package um.edu.uy.business.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Asiento")
+public class Asiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idAsiento;
+
+    @Column(nullable = false)
+    private int numeroAsiento;
+
+    @Column(nullable = true)
+    private long Pasaporte;
+    @Column(nullable = true)
+    private boolean checkIn;
+    @Column(nullable = true)
+    private boolean boarded;
+
+    @OneToMany(targetEntity = Maleta.class, cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "RelacionMaletaAsiento",
+            joinColumns = {@JoinColumn(
+                    name = "id_asiento"
+            )},
+            inverseJoinColumns = {@JoinColumn(
+                    name = "id_maleta"
+            )}
+    )
+    private List<Maleta> maletas;
+
+
+    public Asiento(int numeroAsiento) {
+        this.numeroAsiento = numeroAsiento;
+    }
+
+    public long getIdAsiento() {
+        return idAsiento;
+    }
+
+    public int getNumeroAsiento() {
+        return numeroAsiento;
+    }
+
+    public long getPasaporte() {
+        return Pasaporte;
+    }
+
+    public List<Maleta> getMaletas() {
+        return maletas;
+    }
+
+    public void setMaletas(List<Maleta> maletas) {
+        this.maletas = maletas;
+    }
+
+    public void setPasaporte(long pasaporte) {
+        Pasaporte = pasaporte;
+    }
+}
